7552b4f37eaa72621bd2a1386c83214bf465e840
Gabriel Cristian Circiu <gaci@itu.dk>
2024-03-02 18:21:28 +0100

    Day 1 part 2 near-solution in Python

--- day1_part2/trebuchet.py
+++ day1_part2/trebuchet.py
@@ -3,20 +3,24 @@ import os
 def read_file(file_name):
     with open(file_name, 'r') as file:
         lines = file.readlines()
-    return [list(line.strip()) for line in lines]
-
-def find_digit(line, direction):
-    for char in line[::direction]:
-        if char.isdigit():
-            return char
-    return '0'
+    return [line.strip() for line in lines]
 
 def main():
+    digits_table = { 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6,
+        'seven': 7, 'eight': 8, 'nine': 9, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6,
+        '7': 7, '8': 8, '9': 9 }
     total = 0
     lines = read_file('problem_input')
+
     for line in lines:
-        total += int(find_digit(line, 1) + find_digit(line, -1))
+        indexes = []
+        for (digit, value) in digits_table.items():
+            if line.find(digit) != -1:
+                indexes.append((line.index(digit), value))
+        indexes.sort()
+        total += int(f"{indexes[0][1]}{indexes[-1][1]}")
+
     print(total)
 
 if __name__ == '__main__':
-    main()  # Prints 55447, which is the correct answer
+    main()  # Answer should be 54706 but is returning 54761
