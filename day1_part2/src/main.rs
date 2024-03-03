a385dbc36c35abc36aa3e4c0cef6fee61e68ddf8
Gabriel Cristian Circiu <gaci@itu.dk>
2024-03-03 09:15:17 +0100

    Day 1 part 2 - Python Solution

--- day1_part2/src/main.rs
+++ day1_part2/src/main.rs
@@ -1,16 +1,5 @@
 use std::fs::read_to_string;
 
-fn main() {
-    let lines = read_lines("problem_input");
-    let mut final_value = 0;
-    for line in lines {
-        final_value += (find_number(line.clone()) + &find_number(line.chars().rev().collect()))
-            .parse::<i32>()
-            .unwrap();
-    }
-    println!("{final_value}"); // Prints 55447, which is the correct answer
-}
-
 fn read_lines(filename: &str) -> Vec<String> {
     read_to_string(filename)
         .unwrap()
@@ -26,4 +15,14 @@ fn find_number(s: String) -> String {
         }
     }
     "0".to_string()
+}
+
+fn main() {
+    let mut total = 0;
+    for line in read_lines("problem_input") {
+        total += (find_number(line.clone()) + &find_number(line.chars().rev().collect()))
+            .parse::<i32>()
+            .unwrap();
+    }
+    println!("{total}"); // Prints 55447, which is the correct answer
 }
--- day1_part2/trebuchet.py
+++ day1_part2/trebuchet.py
@@ -2,25 +2,25 @@ import re
 
 def read_file(file_name):
     with open(file_name, 'r') as file:
-        lines = file.readlines()
-    return [line.strip() for line in lines]
+        return [line for line in file.readlines()]
+
+def find_digits(line, digits_table):
+    indexes = []
+    for (digit, value) in digits_table.items():
+        if line.find(digit) != -1:
+            for m in re.finditer(digit, line):
+                indexes.append((m.start(), value))
+    indexes.sort()
+    return indexes[0][1], indexes[-1][1]
 
 def main():
-    digits_table = { 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6,
-        'seven': 7, 'eight': 8, 'nine': 9, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6,
-        '7': 7, '8': 8, '9': 9 }
+    digits_table = { 'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+        'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, '0': 0, '1': 1, '2': 2, '3': 3,
+        '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9 }
     total = 0
-    lines = read_file('problem_input')
-
-    for line in lines:
-        indexes = []
-        for (digit, value) in digits_table.items():
-            if line.find(digit) != -1:
-                for m in re.finditer(digit, line):
-                    indexes.append((m.start(), value))
-        indexes.sort()
-        total += int(f"{indexes[0][1]}{indexes[-1][1]}")
-
+    for line in read_file('problem_input'):
+        first, last = find_digits(line, digits_table)
+        total += int(f"{first}{last}")
     print(total)
 
 if __name__ == '__main__':
